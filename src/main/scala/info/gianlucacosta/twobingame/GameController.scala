30a0dbec8b527f8263735b5895369667a3e958eb
Gianluca Costa <gianluca@gianlucacosta.info>
2017-02-15 04:30:35 +0100

    Update copyright headers

--- src/main/scala/info/gianlucacosta/twobingame/GameController.scala
+++ src/main/scala/info/gianlucacosta/twobingame/GameController.scala
@@ -2,7 +2,7 @@
   ===========================================================================
   TwoBinGame
   ===========================================================================
-  Copyright (C) 2016 Gianluca Costa
+  Copyright (C) 2016-2017 Gianluca Costa
   ===========================================================================
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as
